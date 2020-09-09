38776822602836a3d51a0d69ea0d710f85fe0c4c
pavel <natitshow@gmail.com>
2020-09-09 22:54:28 +0300

    added setResizable(false);

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
         primaryStage.setTitle("Calc");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 235, 300));
         primaryStage.show();
     }
